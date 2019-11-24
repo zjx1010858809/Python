ddea746c30e94237fbc1465c3a17b057ab5b6c7a
zhangjxm <zhangjxm@yonyou.com>
2019-11-24 23:43:20 +0800

    第一天测试

---
+++ test/zhang1.py
@@ -0,0 +1,26 @@
+import turtle
+
+def main():
+
+    turtle.pensize(4)
+    turtle.pencolor('red')
+
+
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(100)
+    turtle.right(135)
+    turtle.forward(50)
+
+    turtle.mainloop()
+
+if __name__ == '__main__':
+    main()
